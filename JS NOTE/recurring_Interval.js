65014ee84320e41a4b5266022676a154f3d7a678
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-02-01 05:08:26 +0600

    Update recurring_Interval.js

--- JS NOTE/recurring_Interval.js
+++ JS NOTE/recurring_Interval.js
@@ -1,17 +1,8 @@
-    async function updateKeywords() {
+  async function updateKeywords() {
         const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+        // Check if there are any rows in the containers
         const pendingContainer = document.getElementById('pending_keywords_container');
         const runningContainer = document.getElementById('running_keywords_container');
-
-        if(pendingContainer){
-            shouldContinueFetching = true;
-        }
-        else if(runningContainer){
-            shouldContinueFetching = true;
-        }else{
-            shouldContinueFetching = false;
-        }
-
         try {
             const response = await fetch('{% url "info_bulk_posting" %}', {
                 headers: {
@@ -21,22 +12,33 @@
             });
     
             const data = await response.json();
-    
-            document.getElementById('pending_keywords_container').innerHTML = data.keyword_pending;
-            document.getElementById('running_keywords_container').innerHTML = data.running_keyword;
+            if (data.running_keyword != "\n" || data.keyword_pending != "") {
+                console.log('Task Alive...')
+                if (data.running_keyword) {
+                    runningContainer.innerHTML = data.running_keyword;
+                } else {
+                    runningContainer.innerHTML = '<tbody id="running_keywords_container"></tbody>';
+                }
+            
+                if (data.keyword_pending) {
+                    pendingContainer.innerHTML = data.keyword_pending;
+                } else {
+                    pendingContainer.innerHTML = '<tbody id="pending_keywords_container"></tbody>';
+                }
+            } else {
+                runningContainer.innerHTML = '<tbody></tbody>';
+                pendingContainer.innerHTML = '<tbody></tbody>';
+                console.log('Finish All Pending Taks..')
+                clearInterval(fetchInterval);
+            }
         } catch (error) {
             console.error('Error fetching data:', error);
         }
-        if (!shouldContinueFetching) {
-            clearInterval(fetchInterval);  // Stop the periodic fetching
-            return;
-        }
-    
     }
     
     // Start periodic fetching
     const fetchInterval = setInterval(() => {
         updateKeywords().catch(error => console.error('Error in interval:', error));
-    }, 1000);
+    }, 1500);
     
     updateKeywords().catch(error => console.error('Error in initial call:', error));
