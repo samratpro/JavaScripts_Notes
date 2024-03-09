98d4a529c4aca9533018bf8fac4fddb5999b58b0
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-03-10 02:09:40 +0600

    Create home.jsx

---
+++ 03. React Note/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";     // Importing ` React `
+
+import '../App.css'            // Importing CSS file `.. ` two dot means one back folder
+
+
+function Home(){
+    return(
+          <div className="container">
+             <h1> This data from Home Page </h1>
+          </div>
+    )
+}
+
+
+export default Home            // export default ` function name ` 
