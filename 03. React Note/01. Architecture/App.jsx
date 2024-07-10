83513802f5b70832864afa56551664da8bf20ff6
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-07-11 03:57:43 +0600

    Update App.jsx

--- 03. React Note/01. Architecture/App.jsx
+++ 03. React Note/01. Architecture/App.jsx
@@ -1,7 +1,4 @@
 import './App.css'
-import HandleClick from './evenHandles'
-import test from './another'
-import Test from './another'
 import AnotherFun from './another'
 function App() {
 
@@ -9,8 +6,6 @@ function App() {
     <>
       <h1> Hello from App JSX </h1>
       <AnotherFun></AnotherFun>
-      <button onClick={HandleClick}>Click Me</button>
-
     </>
 
   )
