131805fa4d23bd2e7b64d80367963a14250dc00e
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-07-11 03:35:17 +0600

    Create App.jsx

---
+++ 03. React Note/EvenHandle/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HandleClick from './evenHandles'
+function App() {
+
+  return (
+    <>
+      <button onClick={HandleClick}>Click Me</button>
+
+    </>
+
+  )
+}
+
+
+export default App
