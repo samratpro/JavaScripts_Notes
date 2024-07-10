b9ff10296050c42f6a1e89a69ccfd91249d76203
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-07-11 02:26:30 +0600

    Create 02.BOM.js

---
+++ 01. JS Practice/02.BOM.js
@@ -0,0 +1,6 @@
+// LocalStorage
+localStorage. getItem('key')   // get a item
+localStorage.setItem('key','data')  // set an item
+localStorage.setItem('person',JSON.stringify({name:'samrat2',age:29}))  // set object item as a string
+localStorage.removeItem('key')  // Remove a single item
+localStorage.clear()  // Remove all item
