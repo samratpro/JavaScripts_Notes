04081f4cec5fdaa7bf1f959bb8cfef12e05247da
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-07-11 03:52:58 +0600

    Create another.jsx

---
+++ 03. React Note/01. Architecture/another.jsx
@@ -0,0 +1,17 @@
+export default function AnotherFun(){
+
+    return(
+        <>
+          <h2> Hello from Another </h2>
+        </>
+
+    )
+
+}
+
+/*
+function AnotherFun(){
+   return <>Hello from Another</>
+}
+export default AnotherFun
+*/
